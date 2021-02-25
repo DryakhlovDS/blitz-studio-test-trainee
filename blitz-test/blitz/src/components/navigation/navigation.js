90825e7ee61d79f17350343edf747d969b1323f9
DryakhlovDS <dryakhlovds@gmail.com>
2021-02-25 15:24:21 +0300

    add layout mobile

--- blitz-test/blitz/src/components/navigation/navigation.js
+++ blitz-test/blitz/src/components/navigation/navigation.js
@@ -3,16 +3,16 @@ import "./navigation.scss";
 function Nav() {
   return (
     <nav className='nav'>
-      <a href='/' className='nav__link'>
+      <a href='#noscroll' className='nav__link'>
         home
       </a>
-      <a href='/' className='nav__link'>
+      <a href='#noscroll' className='nav__link'>
         about me
       </a>
-      <a href='/' className='nav__link'>
+      <a href='#noscroll' className='nav__link'>
         portfolio
       </a>
-      <a href='/' className='nav__link'>
+      <a href='#noscroll' className='nav__link'>
         contact
       </a>
     </nav>
